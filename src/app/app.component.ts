a68cdfefb2d5037960a472ba19dec8f0ba9c39c3
BensonFeng <bensonfeng@outlook.com>
2022-05-05 23:01:23 -0400

    call function on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularjs-practice';
+  data = 'Hello there';
+  num = 100;
+  getName(name: string) {
+    alert(name);
+  }
 }
